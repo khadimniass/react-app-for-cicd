6d040fb9e8dde2f158212e6ce2429dcc6790395b
khadimniass <niassssn@gmail.com>
2025-03-17 00:00:07 +0000

    add new branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React about this new way to reload.
         </a>
       </header>
     </div>
